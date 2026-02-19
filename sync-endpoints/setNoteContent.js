1c9bce89b9617e95e68916047a288939bb556dd8
Aaron Santiago <me@aaron.work>
2026-02-18 21:50:20 -0500

    working versions of endpoints for topic sync

---
+++ sync-endpoints/setNoteContent.js
@@ -0,0 +1,17 @@
+const {req, res} = api;
+const {secret, noteId, content} = req.body;
+
+if (req.method == 'POST' && secret === 'secret-password') {
+
+    let note = api.getNote(noteId);
+
+    note.setContent(content);
+    note.save();
+
+    let notePojo = note.getPojo();
+    notePojo.content = note.getContent();
+    res.status(201).json(notePojo);
+}
+else {
+    res.send(400);
+}
---
+++ sync-endpoints/topicNoteMeta.js
@@ -0,0 +1,29 @@
+const { req, res } = api;
+const { secret } = req.body;
+
+function getChildrenPojo(note, notePojo, depth = 1) {
+  if (depth <= 0) return notePojo;
+  let children = [];
+  for (let child of note.children) {
+    let childPojo = child.getPojo();
+    childPojo = getChildrenPojo(child, childPojo, depth - 1);
+    childPojo.attributes = {};
+    for (let attribute of child.ownedAttributes) {
+      childPojo.attributes[attribute.name] = attribute.value;
+    }
+    children.push(childPojo);
+  }
+  notePojo.children = children;
+  return notePojo;
+}
+
+if (req.method == "POST" && secret === "ultra-secret-password") {
+  const targetParentNote = api.currentNote.getRelation("targetNote").targetNote;
+
+  let notePojo = targetParentNote.getPojo();
+  notePojo = getChildrenPojo(targetParentNote, notePojo, 2);
+
+  res.status(201).json(notePojo);
+} else {
+  res.send(400);
+}
