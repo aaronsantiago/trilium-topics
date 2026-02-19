7f121b777e53bec1a34918a65964a65e83019857
Aaron Santiago <me@aaron.work>
2026-02-18 21:55:03 -0500

    move secrets to attribute

--- sync-endpoints/getNoteContent.js
+++ sync-endpoints/getNoteContent.js
@@ -1,7 +1,7 @@
 const { req, res } = api;
 const { secret, noteId } = req.body;
 
-if (req.method == "POST" && secret === "secret-password") {
+if (req.method == "POST" && secret === api.currentNote.getLabel("secret").value) {
   let note = api.getNote(noteId);
   let notePojo = note.getPojo();
   notePojo.content = note.getContent();
