a516c356abce4582fa6bff302110e99fd79ae7ae
Aaron Santiago <me@aaron.work>
2026-02-19 23:15:06 -0500

    working note sync into topic-editor

--- sync-endpoints/createNote.js
+++ sync-endpoints/createNote.js
@@ -1,7 +1,14 @@
 const { req, res } = api;
 const { secret, title, content, topics, dateCreated } = req.body;
 
-if (req.method == "POST" && secret === api.currentNote.getLabel("secret").value) {
+if (req.method == "OPTIONS") {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  console.log("Received OPTIONS request, sent CORS headers");
+  res.send(200);
+}
+else if (req.method == "POST" && secret === api.currentNote.getLabel("secret").value) {
   let dayNote = api.getDayNote(dateCreated);
 
   const {note} = api.createTextNote(dayNote.noteId, title, content);
