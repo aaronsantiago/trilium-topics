d6228888c9c9ea64612184387dc33ca61bae02d2
Aaron Santiago <me@aaron.work>
2026-02-24 18:12:58 -0500

    move keyboard logic into separate component

---
+++ topics-editor/src/lib/keyboard/keyboard.svelte
@@ -0,0 +1,174 @@
+<script>
+  import { topicsDbState } from "$lib/topicsDb.svelte.js";
+  import WordRing from "./wordRing.svelte";
+  import { addInputListener, addAxisListener } from "$lib/inputs.js";
+  import { generateT9Db } from "$lib/t8-engine.js";
+  import * as cheerio from 'cheerio';
+
+  let { onInsertWord, onDeleteWordBackward, onMoveCursor } = $props();
+
+  let t9Db = $state({});
+  let currentString = $state("");
+  let selectedWordIndex = $state(0);
+  let selectedWordRing = $state(0);
+
+  function updateSelectedWordIndex(index) {
+    selectedWordIndex = index;
+  }
+
+  let t9Words = $derived.by(() => {
+    let curT9Db = t9Db;
+    for (let c of currentString) {
+      if (!(c in curT9Db)) {
+        return [];
+      }
+      curT9Db = curT9Db[c];
+    }
+
+    return curT9Db.entries || [];
+  });
+
+  $effect(() => {
+    t9Words;
+
+    selectedWordRing = 0;
+  });
+
+  let wordRings = $derived.by(() => {
+    let rings = [];
+    let remainingWords = [...t9Words];
+
+    console.log(remainingWords);
+
+    while (remainingWords.length > 0) {
+      rings.push(remainingWords.slice(0, 9));
+      remainingWords = remainingWords.slice(9);
+    }
+    console.log(rings);
+    return rings;
+  });
+
+  $effect(() => {
+    (async () => {
+      let wordList = await fetch("/google-10000-english-usa.txt");
+      let text = await wordList.text();
+
+      let notesWordMap = {};
+
+      for (let noteId in topicsDbState.notes) {
+        let note = topicsDbState.notes[noteId];
+
+        if (note.content) {
+          console.log(note.content)
+          const html = cheerio.load(note.content);
+          html("br").replaceWith("\n");
+          html("code").replaceWith("\n");
+
+          html('p, div, h1, h2, h3, h4, h5, h6, li, tr, blockquote, pre').each((_, el) => {
+            html(el).append('\n');
+          });
+
+          let textContent = html.text();
+
+          let words = textContent.split(/\s+/);
+          for (let word of words) {
+            let cleanedWord = word.toLowerCase().replace(/[^a-z]/g, "");
+            if (cleanedWord.length == 0) continue;
+            if (!notesWordMap[cleanedWord]) notesWordMap[cleanedWord] = 0;
+            notesWordMap[cleanedWord]++;
+          }
+        }
+      }
+      let wordsWithCounts = Object.entries(notesWordMap);
+      wordsWithCounts.sort((a, b) => b[1] - a[1]);
+      let noteWordList = wordsWithCounts.map(([word, count]) => word);
+      let noteDictionary = noteWordList.join("\n") + "\n" + text;
+      t9Db = generateT9Db(noteDictionary);
+    })();
+  });
+
+  let wordSelectionXAxis = $state(0);
+  let wordSelectionYAxis = $state(0);
+
+  $effect(() => {
+    return addAxisListener((e) => {
+      if (e.axis == "lx") {
+        wordSelectionXAxis = e.value;
+      }
+      if (e.axis == "ly") {
+        wordSelectionYAxis = e.value;
+      }
+    });
+  });
+
+  $effect(() => {
+    return addInputListener((e) => {
+      if (e == "l2") {
+        currentString += "s";
+      }
+      if (e == "l1") {
+        currentString += "t";
+      }
+      if (e == "r1") {
+        currentString += "n";
+      }
+      if (e == "r2") {
+        currentString += "e";
+      }
+
+      if (e == "l3") {
+        selectedWordRing = (selectedWordRing + 1) % wordRings.length;
+      }
+
+      if (e == "left") {
+        onMoveCursor("left");
+      }
+
+      if (e == "right") {
+        onMoveCursor("right");
+      }
+
+      if (e == "up") {
+        onMoveCursor("up");
+      }
+
+      if (e == "down") {
+        onMoveCursor("down");
+      }
+
+      if (e == "delete") {
+        if (currentString.length > 0) {
+          currentString = currentString.slice(0, -1);
+        } else {
+          onDeleteWordBackward();
+        }
+      }
+
+      if (e == "confirm") {
+        let word = t9Words[selectedWordRing * 9 + selectedWordIndex];
+        if (word) {
+          currentString = "";
+          onInsertWord(word);
+        } else {
+          currentString = "";
+        }
+      }
+    });
+  });
+</script>
+
+{#each wordRings as words, i (words.join("-") + selectedWordRing)}
+  <WordRing
+    selected={i == selectedWordRing}
+    xAxisValue={i == selectedWordRing ? wordSelectionXAxis : 0}
+    yAxisValue={i == selectedWordRing ? wordSelectionYAxis : 0}
+    words={words}
+    selectedWordIndexUpdateCallback={i == selectedWordRing ? updateSelectedWordIndex : null}
+  />
+{/each}
+<div>{currentString}</div>
+<div>
+  {#each t9Words as word, i}
+    <div>{word}</div>
+  {/each}
+</div>
--- topics-editor/src/routes/note/+page.svelte
+++ topics-editor/src/routes/note/+page.svelte
@@ -2,99 +2,16 @@
   import { appState } from "$lib/appState.svelte.js";
   import { topicsDbState } from "$lib/topicsDb.svelte.js";
   import Editor from "$lib/editor.svelte";
-  import WordRing from "./wordRing.svelte";
-  import { addInputListener, addAxisListener } from "$lib/inputs.js";
+  import Keyboard from "$lib/keyboard/keyboard.svelte";
+  import { addInputListener } from "$lib/inputs.js";
   import * as editorActions from "$lib/editorActions.js";
   import { goto } from "$app/navigation";
-  import * as cheerio from 'cheerio';
-
-  import { generateT9Db } from "$lib/t8-engine.js";
-
-  let t9Db = $state({});
-  let currentString = $state("");
 
   let editor = $state(null);
-  let selectedWordIndex = $state(0);
-  let selectedWordRing = $state(0);
-
-  function updateSelectedWordIndex(index) {
-    selectedWordIndex = index;
-  }
 
   function setEditor(e) {
     editor = e;
   }
-
-  let t9Words = $derived.by(() => {
-    let curT9Db = t9Db;
-    for (let c of currentString) {
-      if (!(c in curT9Db)) {
-        return [];
-      }
-      curT9Db = curT9Db[c];
-    }
-
-    return curT9Db.entries || [];
-  });
-
-  $effect(() => {
-    t9Words;
-
-    selectedWordRing = 0;
-  });
-
-  let wordRings = $derived.by(() => {
-    let rings = [];
-    let remainingWords = [...t9Words];
-
-    console.log(remainingWords);
-
-    while (remainingWords.length > 0) {
-      rings.push(remainingWords.slice(0, 9));
-      remainingWords = remainingWords.slice(9);
-    }
-    console.log(rings);
-    return rings;
-  });
-
-  $effect(() => {
-    (async () => {
-      let wordList = await fetch("/google-10000-english-usa.txt");
-      let text = await wordList.text();
-
-      let notesWordMap = {};
-
-      for (let noteId in topicsDbState.notes) {
-        let note = topicsDbState.notes[noteId];
-
-        if (note.content) {
-          console.log(note.content)
-          const html = cheerio.load(note.content);
-          html("br").replaceWith("\n");
-          html("code").replaceWith("\n");
-
-          html('p, div, h1, h2, h3, h4, h5, h6, li, tr, blockquote, pre').each((_, el) => {
-            html(el).append('\n');
-          });
-
-          let textContent = html.text();
-
-          let words = textContent.split(/\s+/);
-          for (let word of words) {
-            let cleanedWord = word.toLowerCase().replace(/[^a-z]/g, "");
-            if (cleanedWord.length == 0) continue;
-            if (!notesWordMap[cleanedWord]) notesWordMap[cleanedWord] = 0;
-            notesWordMap[cleanedWord]++;
-          }
-        }
-      }
-      let wordsWithCounts = Object.entries(notesWordMap);
-      wordsWithCounts.sort((a, b) => b[1] - a[1]);
-      let noteWordList = wordsWithCounts.map(([word, count]) => word);
-      let noteDictionary = noteWordList.join("\n") + "\n" + text;
-      t9Db = generateT9Db(noteDictionary);
-    })();
-  });
 
   let note = $derived.by(() => {
     return topicsDbState.notes[appState.selectedNoteId];
@@ -113,74 +30,28 @@
     }
   }
 
-  let wordSelectionXAxis = $state(0);
-  let wordSelectionYAxis = $state(0);
+  function onInsertWord(word) {
+    if (editor) {
+      editorActions.insertWord(editor, word);
+    }
+  }
 
-  $effect(() => {
-    return addAxisListener((e) => {
-      if (e.axis == "lx") {
-        wordSelectionXAxis = e.value;
-      }
-      if (e.axis == "ly") {
-        wordSelectionYAxis = e.value;
-      }
-    });
-  });
+  function onDeleteWordBackward() {
+    if (editor) {
+      editorActions.deleteWordBackward(editor);
+    }
+  }
+
+  function onMoveCursor(direction) {
+    if (!editor) return;
+    if (direction == "left") editorActions.moveCursorLeft(editor);
+    if (direction == "right") editorActions.moveCursorRight(editor);
+    if (direction == "up") editorActions.moveCursorUp(editor);
+    if (direction == "down") editorActions.moveCursorDown(editor);
+  }
 
   $effect(() => {
     return addInputListener((e) => {
-      if (e == "l2") {
-        currentString += "s";
-      }
-      if (e == "l1") {
-        currentString += "t";
-      }
-      if (e == "r1") {
-        currentString += "n";
-      }
-      if (e == "r2") {
-        currentString += "e";
-      }
-
-      if (e == "l3") {
-        selectedWordRing = (selectedWordRing + 1) % wordRings.length;
-      }
-
-      if (e == "left" && editor) {
-        editorActions.moveCursorLeft(editor);
-      }
-
-      if (e == "right" && editor) {
-        editorActions.moveCursorRight(editor);
-      }
-
-      if (e == "up" && editor) {
-        editorActions.moveCursorUp(editor);
-      }
-
-      if (e == "down" && editor) {
-        editorActions.moveCursorDown(editor);
-      }
-
-      if (e == "delete") {
-        if (currentString.length > 0) {
-          currentString = currentString.slice(0, -1);
-        } else if (editor) {
-          editorActions.deleteWordBackward(editor);
-        }
-      }
-
-      if (e == "confirm") {
-        if (editor) {
-          let word = t9Words[selectedWordRing * 9 + selectedWordIndex];
-          if (word) {
-            currentString = "";
-            editorActions.insertWord(editor, word);
-          }
-        }
-        currentString = "";
-      }
-
       if (e == "cancel") {
         goto(`/topic`);
       }
@@ -190,21 +61,7 @@
 
 <div class="flex gap-4 w-screen">
   <div class="flex-grow">
-    {#each wordRings as words, i (words.join("-") + selectedWordRing)}
-      <WordRing
-        selected={i == selectedWordRing}
-        xAxisValue={i == selectedWordRing ? wordSelectionXAxis : 0}
-        yAxisValue={i == selectedWordRing ? wordSelectionYAxis : 0}
-        words={words}
-        selectedWordIndexUpdateCallback={i == selectedWordRing ? updateSelectedWordIndex : null}
-      />
-    {/each}
-    <div>{currentString}</div>
-    <div>
-      {#each t9Words as word, i}
-        <div>{word}</div>
-      {/each}
-    </div>
+    <Keyboard {onInsertWord} {onDeleteWordBackward} {onMoveCursor} />
   </div>
   <div>
     <div class="">{note?.title}</div>
